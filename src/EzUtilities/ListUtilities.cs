a26c8868ab8f291c6a6315654a570dcf59b49436
Andrew Sun <youlosethegame@live.com>
2013-07-14 21:04:24 +0800

    Added non-generic IList rearranging

--- src/EzUtilities/ListUtilities.cs
+++ src/EzUtilities/ListUtilities.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace EzUtilities
 {
     /// <summary>
-    /// Provides utilities for sorting arrays and lists.
+    /// Provides utilities for sorting and rearranging arrays and lists.
     /// </summary>
     public static class ListUtilities
     {
@@ -85,6 +86,77 @@ namespace EzUtilities
 
             if (targetIndex == originalIndex) return;
             T item = list[originalIndex];
+
+            if (targetIndex > originalIndex)
+            {
+                for (int i = originalIndex; i < targetIndex; ++i)
+                {
+                    list[i] = list[i + 1];
+                }
+            }
+            else
+            {
+                for (int i = originalIndex; i > targetIndex; --i)
+                {
+                    list[i] = list[i - 1];
+                }
+            }
+
+            list[targetIndex] = item;
+        }
+
+        /// <summary>
+        /// Moves an item within a list to a new index.
+        /// </summary>
+        /// <param name="list">The list that contains the item.</param>
+        /// <param name="item">The item to move.</param>
+        /// <param name="targetIndex">The index at which to insert the item at.</param>
+        /// <exception cref="ArgumentNullException">Thrown if list is null.</exception>
+        /// <exception cref="System.InvalidOperationException">Thrown if the item was not found in the list.</exception>
+        /// <exception cref="System.ArgumentOutOfRangeException">Target index is not a valid index in the <see cref="T:System.Collections.Generic.IList`1" />.</exception>
+        /// <exception cref="System.NotSupportedException">The <see cref="T:System.Collections.Generic.IList`1" /> is read-only.</exception>
+        public static void Rearrange(this IList list, object item, int targetIndex)
+        {
+            if (list == null) throw new ArgumentNullException("list");
+
+            int originalIndex = list.IndexOf(item);
+            if (originalIndex == -1) throw new InvalidOperationException();
+            if (targetIndex == originalIndex) return;
+
+            if (targetIndex > originalIndex)
+            {
+                for (int i = originalIndex; i < targetIndex; ++i)
+                {
+                    list[i] = list[i + 1];
+                }
+            }
+            else
+            {
+                for (int i = originalIndex; i > targetIndex; --i)
+                {
+                    list[i] = list[i - 1];
+                }
+            }
+
+            list[targetIndex] = item;
+        }
+
+        /// <summary>
+        /// Moves an item within a list to a new index.
+        /// </summary>
+        /// <param name="list">The list that contains the item.</param>
+        /// <param name="originalIndex">The index of the item to move.</param>
+        /// <param name="targetIndex">The index at which to insert the item at.</param>
+        /// <exception cref="ArgumentNullException">Thrown if list is null.</exception>
+        /// <exception cref="System.IndexOutOfRangeException">Source index does not exist in the list.</exception>
+        /// <exception cref="System.ArgumentOutOfRangeException">Target index is not a valid index in the <see cref="T:System.Collections.Generic.IList`1" />.</exception>
+        /// <exception cref="System.NotSupportedException">The <see cref="T:System.Collections.Generic.IList`1" /> is read-only.</exception>
+        public static void RearrangeIndex(this IList list, int originalIndex, int targetIndex)
+        {
+            if (list == null) throw new ArgumentNullException("list");
+
+            if (targetIndex == originalIndex) return;
+            object item = list[originalIndex];
 
             if (targetIndex > originalIndex)
             {
